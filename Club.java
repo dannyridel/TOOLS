2aba562166103fed4e7a819b3d5aa7ac3922458a
Daniel L <63090804+dannyridel@users.noreply.github.com>
2026-03-20 12:30:18 -0700

    implements clubs functionality

--- Club.java
+++ Club.java
@@ -1,8 +1,16 @@
-
+// any imports
 
 public class Club {
+    private final String name;
     private double elo;
     private int gamesPlayed;
+
+    public Club(String s) {
+        String[] info = s.split(" ");
+        name = info[0];
+        elo = Double.parseDouble(info[1]);
+        gamesPlayed = Integer.parseInt(info[2]);
+    }
 
     public void playMatch(double delta) {
         gamesPlayed++;
@@ -10,12 +18,16 @@ public class Club {
     }
 
     public String toString() {
-        // TODO
-        return super.toString();
+        return String.format("%s  %.2f  %d", name, elo, gamesPlayed);
     }
 
     public void updateElo(double delta) {
         elo += delta;
+    }
+
+    // helper functions for private fields
+    public String getName() {
+        return name;
     }
 
     public double getElo() {
