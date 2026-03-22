249b1512ba785267ba9fca9978ab005c5bdb49c1
Daniel Lu <dnyrdl44@cs.washington.edu>
2026-03-22 00:54:10 -0700

    luchadores...

--- ChatParser.java
+++ ChatParser.java
@@ -22,6 +22,7 @@ public class ChatParser {
             }
 
             if (active) {
+                line = line.replace("Luchadores", "Los Cabos United");
                 pw.println(line);
             }
         }
